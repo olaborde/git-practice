282dc58ff00ea97ca563c39ed09ad40aa81823c4
olaborde <oml007@live.com>
2018-07-18 03:30:56 -0400

    added learn javascript folder

---
+++ Learn-javascript/02.js-basics/main.js
@@ -0,0 +1,24 @@
+//Create a String and console.log it
+let carMake = 'Chevrolet';
+let carModel = 'Malibu'
+console.log(carMake);
+//Add two strings together
+console.log(`I owned a ${carMake} ${carModel}`)
+//Create a Number and console.log it
+let number1 = 90;
+let number2 = 89;
+//Add two numbers together
+sumOfTwoNumbers = number1 + number2;
+ console.log(sumOfTwoNumbers)
+ console.log(typeof(sumOfTwoNumbers))
+//Subtract one number from another number.
+console.log(number1 - number2)
+//Multiply two numbers.
+console.log(number1 * number2)
+//Divide one number by another number
+console.log(number1 / number2)
+//Create a Boolean and console.log it
+isHuman = true;
+
+console.log(isHuman)
+console.log(typeof(isHuman))
