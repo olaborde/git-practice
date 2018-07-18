8ecfe04b9389ff9b5ebe984531c9319a05608875
olaborde <oml007@live.com>
2018-07-17 23:35:58 -0400

    even or odd

--- main2.py
+++ main2.py
@@ -8,3 +8,11 @@ monthly_income = int(input("What is your gross monthly income?"))
 # Find the max rent by uncommenting the line below and adding your processing code on the right side
 max_rent = monthly_income * .20
 print("The most you should pay in monthly rent is", max_rent)
+
+
+x = 15
+
+if x % 2 == 0:
+    print(x, "is even")
+else:
+    print(x, "is odd")
