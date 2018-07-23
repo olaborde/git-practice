54c58f40dd7c4377716d4b881293cc2d64816672
olaborde <oml007@live.com>
2018-07-23 16:33:45 -0400

    exercise 1 done

--- Learn-javascript/02.js-basics/arrow-functions.js
+++ Learn-javascript/02.js-basics/arrow-functions.js
@@ -3,6 +3,9 @@
 // For this lesson, do the following:
 
 // Make a function named ten that takes in zero arguments and return the value 10. Try using both () and _ syntax.
+const ten = _ => console.log(10) // with ()
+
+ten()
 // Make a function named logger that takes in one argument. It logs the argument you passed into it. Try it with and without parenthesis ().
 // Make a function called add that adds two numbers together. Try it with and without implicit returns
 // Make a function called multiply that multiplies two numbers together. Try it with and without implicit returns
