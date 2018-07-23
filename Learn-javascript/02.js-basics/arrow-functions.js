52d2ba01594f4c864aafb68cccc93635dc471504
olaborde <oml007@live.com>
2018-07-23 16:23:59 -0400

    js file setup

---
+++ Learn-javascript/02.js-basics/arrow-functions.js
@@ -0,0 +1,8 @@
+// Arrow functions are important. You'll see more and more arrow functions in the future. We're also going to use them a lot in this course. Make sure you're familiar with them.
+
+// For this lesson, do the following:
+
+// Make a function named ten that takes in zero arguments and return the value 10. Try using both () and _ syntax.
+// Make a function named logger that takes in one argument. It logs the argument you passed into it. Try it with and without parenthesis ().
+// Make a function called add that adds two numbers together. Try it with and without implicit returns
+// Make a function called multiply that multiplies two numbers together. Try it with and without implicit returns
