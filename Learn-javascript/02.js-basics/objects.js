b5483cacf98ca98b8dd2bdc7c3cea57706634432
olaborde <oml007@live.com>
2018-08-05 00:12:54 -0400

    empty object

---
+++ Learn-javascript/02.js-basics/objects.js
@@ -0,0 +1,12 @@
+// Practice making objects. You need to use them a lot when you code for real. Do the following:
+
+// Make an empty object
+const empty_object = {}
+// Make a property for your object that can be accessed with a dot notation.
+// Make a property for your object that can only be accessed with the bracket notation.
+// Get the value of a property with the dot notation
+// Get the value of a property with the square bracket notation
+// Set the value of a property with the dot notation
+// Set the value of a property with the square bracket notation
+// Make a method. Call this method
+// Make a method that takes in an argument. Call this method
