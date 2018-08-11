7f411d9dd4c4fe1a6e7209692421bcf12498311f
olaborde <oml007@live.com>
2018-08-11 06:19:55 -0400

    JS querySelector exercise

---
+++ Learn-javascript/02.js-basics/querySelector.js
@@ -0,0 +1,15 @@
+
+/* Practice selecting Elements with document.querySelector and Element.querySelector. Try using ids, classes, tags and attribute selectors as you select from the following HTML.
+
+<ul id="star-wars-characters">
+  <li class="character luke" data-type="hero">Luke Skywalker</li>
+  <li class="character yoda" data-type="master">Yoda</li>
+  <li class="character badboy" data-type="villain">Darth Vader</li>
+</ul>
+Get the #star-wars-characters list with id and tag selectors.
+From the #star-wars-characters list, get the following:
+Luke Skywalker with class, tag and attribute selectors
+Yoda with class and attribute selectors
+Darth Vader with class and attribute selectors
+Notice how you can't select Yoda and Darth Vader with tags when you use querySelector.
+*/
